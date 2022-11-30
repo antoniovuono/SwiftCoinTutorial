d79c5c77844e07963c7bf3e6abca4d46ec1638e9
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-11-30 20:13:14 -0300

    Initial Commit

---
+++ SwiftCoinTutorial/SwiftCoinTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinTutorialApp.swift
+//  SwiftCoinTutorial
+//
+//  Created by Antonio Vuono on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
